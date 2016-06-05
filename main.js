a5307bf598302c61aea58dac33c9f6fa60761b5c
Kuba Wasiak <qbawasiak@wp.pl>
2016-06-05 13:16:29 +0200

    3 buttons

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var container = document.getElementById('main-container');
 var button1 = document.getElementById('button-1');
+var button2 = document.getElementById('button-2');
+var button3 = document.getElementById('button-3');
 button1.addEventListener('click', function() {
-  if (container.classList.contains('order1')) {
-    container.classList.remove('order1');
-  } else {
+  // if (container.classList.contains('order1')) {
+    container.classList.remove('order2');
+    container.classList.remove('order3');
+  // } else {
     container.classList.add('order1');
-  }
+  // }
+});
+button2.addEventListener('click', function() {
+    container.classList.remove('order1');
+    container.classList.remove('order3');
+
+    container.classList.add('order2');
+});
+button3.addEventListener('click', function() {
+    container.classList.remove('order1');
+    container.classList.remove('order2');
+
+    container.classList.add('order3');
 });
