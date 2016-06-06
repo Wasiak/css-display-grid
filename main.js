9ab761b7fbb808dccacb9a0676a26677d8a5f8dd
Kuba Wasiak <qbawasiak@wp.pl>
2016-06-06 22:03:46 +0200

    new type of template, query selector

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-var container = document.getElementById('main-container');
-var button1 = document.getElementById('button-1');
-var button2 = document.getElementById('button-2');
-var button3 = document.getElementById('button-3');
+var $ = function(selector) {
+   return document.querySelectorAll(selector);
+}
+var container = $('#main-container')[0];
+var button1 = $('#button-1')[0];
+var button2 = $('#button-2')[0];
+var button3 = $('#button-3')[0];
+var button4 = $('#button-4')[0];
 button1.addEventListener('click', function() {
-  // if (container.classList.contains('order1')) {
     container.classList.remove('order2');
     container.classList.remove('order3');
-  // } else {
+    container.classList.remove('order4');
+
     container.classList.add('order1');
-  // }
 });
 button2.addEventListener('click', function() {
     container.classList.remove('order1');
     container.classList.remove('order3');
+    container.classList.remove('order4');
 
     container.classList.add('order2');
 });
 button3.addEventListener('click', function() {
     container.classList.remove('order1');
     container.classList.remove('order2');
+    container.classList.remove('order4');
 
     container.classList.add('order3');
+});
+button4.addEventListener('click', function() {
+    container.classList.remove('order1');
+    container.classList.remove('order2');
+    container.classList.remove('order3');
+
+    container.classList.add('order4');
 });
