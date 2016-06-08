51bf85721f9acd5a69d2edb7e80b43cf6b00d3ee
Kuba Wasiak <qbawasiak@wp.pl>
2016-06-08 22:00:19 +0200

    prototype of one element

--- main.js
+++ main.js
@@ -68,18 +68,35 @@ button4.addEventListener('click', function() {
 var setItemPositionAbs = function(element) {
   var item = getItemPosition(element);
   console.log('item position', item);
+
+  var bgColor = window.getComputedStyle(element, null).getPropertyValue("background-color");
   element.classList.add('hidden');
-  element.style.left = item[0] + 'px';
-  element.style.top = item[1] + 'px';
-  element.style.width = item[2] + 'px';
-  element.style.height = item[3] + 'px';
+  var tempItem = document.createElement('div');
+  container.appendChild(tempItem);
+  tempItem.classList.add('temp-item');
+  tempItem.id = 'test-temp-item';
+
+  tempItem.style.backgroundColor = bgColor;
+
+  tempItem.style.left = item[0] + 'px';
+  tempItem.style.top = item[1] + 'px';
+  tempItem.style.width = item[2] + 'px';
+  tempItem.style.height = item[3] + 'px';
 }
-var backToGridPosition = function(element) {
-  item1.classList.remove('hidden');
-  element.style.left = 'auto';
-  element.style.top = 'auto';
-  element.style.width = 'auto';
-  element.style.height = 'auto';
+var setNewItemPositionAbs = function(element) {
+  var item = getItemPosition(element);
+
+  var tempItem = $('#test-temp-item')[0];
+  tempItem.style.left = item[0] + 'px';
+  tempItem.style.top = item[1] + 'px';
+  tempItem.style.width = item[2] + 'px';
+  tempItem.style.height = item[3] + 'px';
+
+  tempItem.addEventListener('transitionend', function() {
+    item1.classList.remove('hidden');
+    container.removeChild(tempItem);
+  });
+
 }
 
 button5.addEventListener('click', function() {
@@ -91,12 +108,9 @@ button5.addEventListener('click', function() {
   container.classList.remove('order3');
   container.classList.remove('order2');
   container.classList.remove('order4');
-  
-  item1.addEventListener('transitionend', function() {
-    backToGridPosition(item1);
-    container.classList.add('order1');
-    setItemPositionAbs(item1);
-  });
+
+  container.classList.add('order1');
+  setNewItemPositionAbs(item1);
   
   // getItemPosition(item1);
   // item1.classList.remove('hidden');
